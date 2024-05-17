e559dc859d3b78cdd5ed0a7271d4a3d46a6f84d3
VeryBearyAndy <Crecentbear@gmail.com>
2024-05-17 09:31:14 -0400

    add more documentation tags that help swagger produce more documentation in the API landing page

--- dotnet/API/Controllers/ProductsController.cs
+++ dotnet/API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -34,10 +35,18 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> getProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productsRepo.GetEntityWithSpec(spec);
+            
+            if(product == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
             return Mapper.Map<Product, ProductToReturnDto>(product);
         }
 
