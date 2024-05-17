d9afd5e92cfffaafcab561e30eecbe12f675cb5e
VeryBearyAndy <Crecentbear@gmail.com>
2024-05-17 08:56:20 -0400

    Add custom exception handling on the API level by creating a ApiExceptions class and creating a middleware Class to catch unhandled axceptions as they're throun in the program.CS file

--- dotnet/API/Program.cs
+++ dotnet/API/Program.cs
@@ -1,3 +1,4 @@
+using API.middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 if (app.Environment.IsDevelopment())
