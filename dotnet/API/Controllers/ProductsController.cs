d0993356907e343baddf09f9e1afbd4130244bed
VeryBearyAndy <Crecentbear@gmail.com>
2024-05-17 20:53:34 -0400

    support sorting by typeID or by BrandId

--- dotnet/API/Controllers/ProductsController.cs
+++ dotnet/API/Controllers/ProductsController.cs
@@ -26,9 +26,10 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> getProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> getProducts(
+            string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(Mapper
                 .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
