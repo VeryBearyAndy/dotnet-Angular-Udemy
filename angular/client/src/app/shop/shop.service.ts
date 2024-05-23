6782a99331995b02d47264197cb40bb1325b4c68
VeryBearyAndy <Crecentbear@gmail.com>
2024-05-22 22:29:20 -0400

    add http request method for products into shop.service.ts

--- angular/client/src/app/shop/shop.service.ts
+++ angular/client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagenation } from '../shared/models/pagenation';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = "https://localhost:5001/api/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Pagenation<Product[]>>(this.baseUrl + 'products');
+  }
 }
