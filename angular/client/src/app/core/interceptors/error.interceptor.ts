130c750cb787570c5136de29ec4467bb98acc04f
VeryBearyAndy <Crecentbear@gmail.com>
2024-05-27 22:13:46 -0400

    inject toastr into the error.interceptor.ts file

--- angular/client/src/app/core/interceptors/error.interceptor.ts
+++ angular/client/src/app/core/interceptors/error.interceptor.ts
@@ -8,11 +8,12 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr : ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
