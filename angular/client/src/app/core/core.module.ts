4418d95a7e6957defd7c7bb13dcd803aab63578b
VeryBearyAndy <Crecentbear@gmail.com>
2024-05-28 22:14:58 -0400

    export the app-section-header in the core module

--- angular/client/src/app/core/core.module.ts
+++ angular/client/src/app/core/core.module.ts
@@ -27,7 +27,8 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
     RouterModule
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
