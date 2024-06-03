663b62ac89600c9f5f33327dd6f650f3b0c43364
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-03 17:03:02 -0400

    intialize the redis database in the Basket Repository

--- dotnet/Infrastructure/Data/BasketRepository.cs
+++ dotnet/Infrastructure/Data/BasketRepository.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 
 namespace Infrastructure.Data
 {
     public class BasketRepository : IBasketRepository
     {
+        private readonly IDatabase _database;
+        
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
         public Task<bool> DeleteBasketAsync(string basketId)
         {
             throw new NotImplementedException();
