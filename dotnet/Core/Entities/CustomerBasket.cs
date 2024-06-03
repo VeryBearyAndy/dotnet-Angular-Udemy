3877fb2c45008b896ebf896b422e20ca51fa716b
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-03 16:31:20 -0400

    add constructor to CustomerBasket

--- dotnet/Core/Entities/CustomerBasket.cs
+++ dotnet/Core/Entities/CustomerBasket.cs
@@ -7,7 +7,14 @@ namespace Core.Entities
 {
     public class CustomerBasket
     {
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+
+        public CustomerBasket(){}
+
         public string Id { get; set; }
-        public List<BasketItem> Items { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
     }
 }
