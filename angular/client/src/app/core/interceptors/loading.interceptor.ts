ec7f7482aed394e8c5c8e32288630025decf6a27
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-03 14:42:05 -0400

    add the code to use the spinner in the base applicaition modules

--- angular/client/src/app/core/interceptors/loading.interceptor.ts
+++ angular/client/src/app/core/interceptors/loading.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, delay } from 'rxjs';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -13,6 +13,8 @@ export class LoadingInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      delay(1000)
+    )
   }
 }
