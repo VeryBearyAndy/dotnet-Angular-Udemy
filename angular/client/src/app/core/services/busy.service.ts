1294f60ce5b297367b25235d82965af66a9a2f44
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-03 14:48:42 -0400

    add the code to apply the spinner to the busy.service.ts

--- angular/client/src/app/core/services/busy.service.ts
+++ angular/client/src/app/core/services/busy.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusyService {
+  busyRequestCount = 0;
 
-  constructor() { }
+  constructor(private spinnerServuce: NgxSpinnerService) { }
+
+  busy() {
+    this.busyRequestCount++;
+    this.spinnerServuce.show(undefined, {
+      type: 'timer',
+      bdColor: "rgba(255,255,255,0.7)",
+      color: "#333333"
+    });
+  }
+
+  idle(){
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0){
+      this.busyRequestCount = 0;
+      this.spinnerServuce.hide();
+    }
+  }
+
 }
