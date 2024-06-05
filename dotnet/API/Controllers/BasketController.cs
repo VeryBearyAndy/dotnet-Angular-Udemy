67a9696711da95873584974301c786623a225524
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-04 22:57:57 -0400

    add the endpoints to basketController that implement the basketRepository

--- dotnet/API/Controllers/BasketController.cs
+++ dotnet/API/Controllers/BasketController.cs
@@ -10,6 +10,28 @@ namespace API.Controllers
 {
     public class BasketController : BaseApiController
     {
+            public IBasketRepository _basketRepository { get; }
+            public BasketController(IBasketRepository basketRepository)
+            {
+                _basketRepository = basketRepository;
+            }
 
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedBasket);
+        }
+        [HttpDelete]
+        public async Task DeleteBasketAsync(String id)
+        {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
     }
 }
