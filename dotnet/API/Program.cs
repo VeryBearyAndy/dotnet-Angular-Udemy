cbcd404fef29643f1b4614f7820e9f37ecea60d6
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-17 20:21:25 -0400

    IdentityServicesExtnesion and add it in the program.cs file

--- dotnet/API/Program.cs
+++ dotnet/API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
