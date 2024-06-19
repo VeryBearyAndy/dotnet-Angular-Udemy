1b8d3e2341c92a5c7d1c309df5bae61cb23db1c6
VeryBearyAndy <Crecentbear@gmail.com>
2024-06-18 21:57:00 -0400

    add some shit, I really don't know

--- dotnet/API/Extensions/IdentityServiceExtensions.cs
+++ dotnet/API/Extensions/IdentityServiceExtensions.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities.identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -14,6 +16,15 @@ namespace API.Extensions
             services.AddDbContext<AppIdentityDbContext>(opt => {
                 opt.UseSqlite(Config.GetConnectionString("IdentityConnection"));
             });
+
+            services.AddIdentityCore<AppUser>(opt => {
+                //can add identity option here
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            
+
             return services;
         }
     }
